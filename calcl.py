11d666a5118c96027718e6d666b1b7e2b7cc4e0a
Dipesh Karki <56595355+lunnar211@users.noreply.github.com>
2024-06-06 23:18:27 +0545

    Create calcl.py
    this is while loop simple calculator

---
+++ calcl.py
@@ -0,0 +1,20 @@
+running=True
+while running:
+    a=int(input("Enter the first numer: "))
+    b=int(input("Enter the second number: "))
+    c=input("Enter the symbol: ")
+
+    if c == "+":
+        print("The sum of a+b is ",a+b)
+    elif c =="-":
+        print("The difference of a-b is",a-b)
+    elif c == "*":
+        print("the multiplication of a*b is  ",a*b)
+    elif c == "/":
+        print("The division of a/b is ",a/b)
+    else:
+        print("Try again ")
+    play=input("Do you want to run again (y/n)".lower())
+if not play=="y":
+    running=False
+print("Thrank you for using our calculator")
